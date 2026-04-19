239267f53a64c7ba59e91dcc7212c65b2e6a9518
Minionguyjpro <minionguyjpro@gmail.com>
2026-04-19 21:33:23 +0200

    Update home_page.h

--- includes/_display/home_page.h
+++ includes/_display/home_page.h
@@ -1,13 +1,17 @@
 #pragma once
 
-void draw_home(Display &it) {
-  it.printf((240 / 2), (140 / 6) * 1 + 5,
-            id(large), id(lcd_green),
-            TextAlign::TOP_CENTER,
-            "%.1f °C", id(temperature).state);
+void draw_home_page(esphome::display::Display &it) {
+  it.printf(240 / 2, (140 / 6) * 1 + 5,
+            id(large).get(),
+            id(lcd_green),
+            esphome::display::TextAlign::TOP_CENTER,
+            "%.1f °C",
+            id(temperature).state);
 
   it.printf(5, (140 / 6) * 3 + 10,
-            id(medium), id(lcd_green),
-            TextAlign::TOP_LEFT,
-            "%.1f °F", id(temperature).state * 9 / 5 + 32);
+            id(medium).get(),
+            id(lcd_green),
+            esphome::display::TextAlign::TOP_LEFT,
+            "%.1f °F",
+            id(temperature).state * 9 / 5 + 32);
 }
