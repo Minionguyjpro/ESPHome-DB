ef7fd97f679e8e41396971a4268e9cde5283d879
Minionguyjpro <minionguyjpro@gmail.com>
2026-04-19 19:41:09 +0200

    Add draw_home function to display temperature

---
+++ includes/display/home_page.h
@@ -0,0 +1,13 @@
+#pragma once
+
+void draw_home(DisplayBuffer &it) {
+  it.printf((240 / 2), (140 / 6) * 1 + 5,
+            id(large), id(lcd_green),
+            TextAlign::TOP_CENTER,
+            "%.1f °C", id(temperature).state);
+
+  it.printf(5, (140 / 6) * 3 + 10,
+            id(medium), id(lcd_green),
+            TextAlign::TOP_LEFT,
+            "%.1f °F", id(temperature).state * 9 / 5 + 32);
+}
