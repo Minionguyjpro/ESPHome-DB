f623018afaa73a42097cf14c124802428f65c6bc
Minionguyjpro <minionguyjpro@gmail.com>
2026-04-19 21:33:05 +0200

    Refactor draw_header function for display updates

--- includes/_display/header.h
+++ includes/_display/header.h
@@ -1,13 +1,22 @@
 #pragma once
 
-void draw_header(Display &it) {
-  it.print(5, 5, id(font20), id(lcd_yellow), "HELLO");
+void draw_header(esphome::display::Display &it) {
+  it.print(5, 5,
+           id(font20).get(),
+           id(lcd_yellow),
+           "HELLO");
 
   if (id(system_status).state) {
-    it.print(235, 5, id(font20), id(lcd_green),
-             TextAlign::TOP_RIGHT, "Online");
+    it.print(235, 5,
+             id(font20).get(),
+             id(lcd_green),
+             esphome::display::TextAlign::TOP_RIGHT,
+             "Online");
   } else {
-    it.print(235, 5, id(font20), id(lcd_red),
-             TextAlign::TOP_RIGHT, "Offline");
+    it.print(235, 5,
+             id(font20).get(),
+             id(lcd_red),
+             esphome::display::TextAlign::TOP_RIGHT,
+             "Offline");
   }
 }
