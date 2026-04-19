cf6272940c15065d4e271fc7909ecf572b225591
Minionguyjpro <minionguyjpro@gmail.com>
2026-04-19 19:40:31 +0200

    Add draw_background function in background.h
    Implement draw_background function to fill the display with a blue rectangle.

---
+++ includes/display/background.h
@@ -0,0 +1,6 @@
+#pragma once
+
+void draw_background(DisplayBuffer &it) {
+  it.rectangle(0, 0, it.get_width(), it.get_height(), id(lcd_blue));
+  it.rectangle(0, 25, it.get_width(), it.get_height(), id(lcd_blue));
+}
