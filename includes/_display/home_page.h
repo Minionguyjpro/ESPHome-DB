f63403f73e37f2d815aa224953e8ea42b72887d4
Minionguyjpro <minionguyjpro@gmail.com>
2026-04-19 22:31:22 +0200

    Adjust home page temperature display positions

--- includes/_display/home_page.h
+++ includes/_display/home_page.h
@@ -2,17 +2,19 @@
 
 void draw_home_page(esphome::display::Display &it) {
 
+  int y = (140 / 6) * 3 + 10;
+  
   // Temperature (C)
-  it.printf(120, 60,
+  it.printf(5, y,
             &id(large),
             id(lcd_green),
             esphome::display::COLOR_OFF,
-            esphome::display::TextAlign::TOP_CENTER,
+            esphome::display::TextAlign::TOP_LEFT,
             "%.1f °C",
             id(temperature).state);
 
   // Temperature (F)
-  it.printf(5, (140 / 6) * 3 + 10,
+  it.printf(5, y + 20,
             &id(medium),
             id(lcd_green),
             esphome::display::COLOR_OFF,
