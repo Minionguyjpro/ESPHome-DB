a9e0f846bb6694d77946ecc09ec46264e7aca7c8
Minionguyjpro <minionguyjpro@gmail.com>
2026-04-19 21:40:18 +0200

    Update draw_display function signature and implementation

--- includes/_display/display.h
+++ includes/_display/display.h
@@ -3,9 +3,10 @@
 #include "background.h"
 #include "header.h"
 #include "home_page.h"
+#include "pages.h"
 
-void draw_display(Display &it) {
+void draw_display(esphome::display::Display &it) {
   draw_background(it);
   draw_header(it);
-  draw_home(it);
+  draw_home_page(it);
 }
