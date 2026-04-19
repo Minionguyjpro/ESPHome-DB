a8e267bd95c3161860de5cf6cf7e3c34e8e9ba8b
Minionguyjpro <minionguyjpro@gmail.com>
2026-04-19 21:31:10 +0200

    Change parameter type from DisplayBuffer to Display

--- includes/_display/pages.h
+++ includes/_display/pages.h
@@ -1,10 +1,10 @@
 #pragma once
 
-void draw_home(DisplayBuffer &it) {
+void draw_home(Display &it) {
   it.printf(120, 60, id(large), "%.1f°C", id(temperature).state);
 }
 
-void draw_page(DisplayBuffer &it, int page) {
+void draw_page(Display &it, int page) {
   switch (page) {
     case 0: draw_home(it); break;
   }
