e83ebc22e243436321fcaf8ae7687fdc60ec50d9
Minionguyjpro <minionguyjpro@gmail.com>
2026-04-19 21:39:55 +0200

    Implement draw_home_page function in pages.h

--- includes/_display/pages.h
+++ includes/_display/pages.h
@@ -1,14 +1,12 @@
 #pragma once
 
-#include "home_page.h"
+void draw_home_page(esphome::display::Display &it) {
+  it.printf(120, 60, id(large), "%.1f°C", id(temperature).state);
+}
 
 void draw_page(esphome::display::Display &it, int page) {
   switch (page) {
     case 0:
-      draw_home_page(it);
-      break;
-
-    default:
       draw_home_page(it);
       break;
   }
