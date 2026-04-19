f0e96e15fb6f0d5f573bbe1ff5c83ad9153580b4
Minionguyjpro <minionguyjpro@gmail.com>
2026-04-19 21:48:29 +0200

    Refactor draw_page function to simplify parameters

--- includes/_display/pages.h
+++ includes/_display/pages.h
@@ -1,11 +1,6 @@
 #pragma once
-
 #include "home_page.h"
 
-void draw_page(esphome::display::Display &it, int page) {
-  switch (page) {
-    case 0:
-      draw_home_page(it);
-      break;
-  }
+void draw_page(esphome::display::Display &it) {
+  draw_home_page(it);
 }
