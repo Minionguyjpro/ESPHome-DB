d02989a4ffde9305232fc4ba995acbd7a738a5d0
Minionguyjpro <minionguyjpro@gmail.com>
2026-04-19 21:36:58 +0200

    Fix display function to use correct font IDs

--- includes/_display/home_page.h
+++ includes/_display/home_page.h
@@ -2,14 +2,14 @@
 
 void draw_home_page(esphome::display::Display &it) {
   it.printf(240 / 2, (140 / 6) * 1 + 5,
-            id(large).get(),
+            id(large),
             id(lcd_green),
             esphome::display::TextAlign::TOP_CENTER,
             "%.1f °C",
             id(temperature).state);
 
   it.printf(5, (140 / 6) * 3 + 10,
-            id(medium).get(),
+            id(medium),
             id(lcd_green),
             esphome::display::TextAlign::TOP_LEFT,
             "%.1f °F",
