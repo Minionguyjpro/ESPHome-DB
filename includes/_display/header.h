f874dc1c81c9fe05b5943f1f6b88900d644bd1ae
Minionguyjpro <minionguyjpro@gmail.com>
2026-04-19 21:36:44 +0200

    Refactor draw_header function to remove unnecessary calls

--- includes/_display/header.h
+++ includes/_display/header.h
@@ -2,19 +2,19 @@
 
 void draw_header(esphome::display::Display &it) {
   it.print(5, 5,
-           id(font20).get(),
+           id(font20),
            id(lcd_yellow),
            "HELLO");
 
   if (id(system_status).state) {
     it.print(235, 5,
-             id(font20).get(),
+             id(font20),
              id(lcd_green),
              esphome::display::TextAlign::TOP_RIGHT,
              "Online");
   } else {
     it.print(235, 5,
-             id(font20).get(),
+             id(font20),
              id(lcd_red),
              esphome::display::TextAlign::TOP_RIGHT,
              "Offline");
