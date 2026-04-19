ba69fade6d94760c74342b97cb3183259185a57e
Minionguyjpro <minionguyjpro@gmail.com>
2026-04-19 19:40:54 +0200

    Add color to header text based on system status

--- includes/display/header.h
+++ includes/display/header.h
@@ -1,11 +1,13 @@
 #pragma once
 
 void draw_header(DisplayBuffer &it) {
-  it.print(5, 5, id(font20), "HELLO");
+  it.print(5, 5, id(font20), id(lcd_yellow), "HELLO");
 
   if (id(system_status).state) {
-    it.print(235, 5, id(font20), TextAlign::TOP_RIGHT, "Online");
+    it.print(235, 5, id(font20), id(lcd_green),
+             TextAlign::TOP_RIGHT, "Online");
   } else {
-    it.print(235, 5, id(font20), TextAlign::TOP_RIGHT, "Offline");
+    it.print(235, 5, id(font20), id(lcd_red),
+             TextAlign::TOP_RIGHT, "Offline");
   }
 }
