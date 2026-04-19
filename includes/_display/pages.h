12b2eb7f2ef5e949691221b86beec21445fce16d
Minionguyjpro <minionguyjpro@gmail.com>
2026-04-19 21:42:28 +0200

    Remove draw_home_page function implementation
    Removed the implementation of draw_home_page function.

--- includes/_display/pages.h
+++ includes/_display/pages.h
@@ -1,8 +1,6 @@
 #pragma once
 
-void draw_home_page(esphome::display::Display &it) {
-  it.printf(120, 60, id(large), "%.1f°C", id(temperature).state);
-}
+#include "home_page.h"
 
 void draw_page(esphome::display::Display &it, int page) {
   switch (page) {
