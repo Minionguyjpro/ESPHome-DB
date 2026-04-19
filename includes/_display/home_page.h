851cf11c91af41b2b54c990c3927fe9e705ee2d7
Minionguyjpro <minionguyjpro@gmail.com>
2026-04-19 21:39:37 +0200

    Fix formatting and add newline at end of home_page.h

--- includes/_display/home_page.h
+++ includes/_display/home_page.h
@@ -4,6 +4,7 @@ void draw_home_page(esphome::display::Display &it) {
   it.printf(240 / 2, (140 / 6) * 1 + 5,
             id(large),
             id(lcd_green),
+            esphome::display::COLOR_OFF,
             esphome::display::TextAlign::TOP_CENTER,
             "%.1f °C",
             id(temperature).state);
@@ -11,6 +12,7 @@ void draw_home_page(esphome::display::Display &it) {
   it.printf(5, (140 / 6) * 3 + 10,
             id(medium),
             id(lcd_green),
+            esphome::display::COLOR_OFF,
             esphome::display::TextAlign::TOP_LEFT,
             "%.1f °F",
             id(temperature).state * 9 / 5 + 32);
