4f9af907fd6b42d057cf3150b2411dac8dc716a1
Minionguyjpro <minionguyjpro@gmail.com>
2026-04-19 22:00:53 +0200

    Fix reference to font20 in draw_header function

--- includes/_display/header.h
+++ includes/_display/header.h
@@ -2,20 +2,20 @@
 
 void draw_header(esphome::display::Display &it) {
   it.print(5, 5,
-           id(font20),
+           &id(font20),
            id(lcd_yellow),
            esphome::display::TextAlign::TOP_LEFT,
            "HELLO");
 
   if (id(system_status).state) {
     it.print(235, 5,
-             id(font20),
+             &id(font20),
              id(lcd_green),
              esphome::display::TextAlign::TOP_RIGHT,
              "Online");
   } else {
     it.print(235, 5,
-             id(font20),
+             &id(font20),
              id(lcd_red),
              esphome::display::TextAlign::TOP_RIGHT,
              "Offline");
